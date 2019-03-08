7e198ea9e0ad4ce69508783f1929246de2c9331f
caumes <caumes.geraldine@gmail.com>
2019-03-08 14:18:03 +0100

    client

--- vol-model/src/main/java/sopra/vol/model/ClientParticulier.java
+++ vol-model/src/main/java/sopra/vol/model/ClientParticulier.java
@@ -1,5 +1,10 @@
 package sopra.vol.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientParticulier")
 public class ClientParticulier extends Client {
 	private String prenom;
 	private Civilite civilite;
